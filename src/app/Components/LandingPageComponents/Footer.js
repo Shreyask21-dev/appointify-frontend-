92a36d372ba98f4e72b5a52d4749d466447fc9c0
admin <admin@gmail.com>
2025-07-19 16:20:21 +0530

    fix links issue

--- src/app/Components/LandingPageComponents/Footer.js
+++ src/app/Components/LandingPageComponents/Footer.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-
+import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className="bg-black">
@@ -8,9 +8,18 @@ const Footer = () => {
     
         {/* Copyright */}
         <div className="w-md-85 text-lg-center mx-lg-auto">
-          <p className="text-white small">
-            &copy;  2025 Copyright © Coinage Inc. All rights reserved
+           <p className="text-white">
+            &copy; 2025 Coinage Inc. All rights reserved.
           </p>
+
+
+          <p>
+            <Link href="/Terms" className="text-light small">Terms and Conditions</Link> |{" "}
+            <Link href="/Cancellation" className="text-light small">Cancellation & Refund Policy</Link> |{" "}
+            <Link href="/Shipping" className="text-light small">Shipping & Delivery Policy</Link> |{" "}
+            <Link href="/PrivacyPolicy" className="text-light small">Privacy Policy</Link> |{" "}
+            <Link href="/Contact" className="text-light small">Contact Us</Link>
+              </p>
           
         </div>
         {/* End Copyright */}
